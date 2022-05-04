fd729335fe788db38f995d768b30c3e4edd1f125
vidhijain248 <vhr1317@gmail.com>
2022-05-04 12:22:29 +0530

    updated in components

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let {title,description,imgUrl,newsUrl,author,date }=this.props;
+    let { title, description, imgUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div>
         <div className="card">
-  <img src={!imgUrl?"https://thehillstimes.in/wp-content/uploads/2022/03/editorial.jpg":imgUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <p className='card-text'><small className='text-muted'>By {author} on {date}</small></p>
-    <a rel='noreferrer' href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
-  </div>
-</div>
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ left:"95%", zIndex:"1" }}
+          >
+            {source}
+          </span>
+          <img
+            src={
+              !imgUrl
+                ? "https://thehillstimes.in/wp-content/uploads/2022/03/editorial.jpg"
+                : imgUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on {date}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
